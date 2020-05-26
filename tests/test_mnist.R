d7a1cab174a0c85a6bb60c4ee8dbb12551a1dac4
MaggieWang0614 <maggiewang@asu.edu>
2020-05-26 11:58:52 -0700

    change parameter

--- tests/test_mnist.R
+++ tests/test_mnist.R
@@ -53,24 +53,24 @@ max_depth = 25
 mtry = 100
 num_cutpoints=20
 
-drop_threshold = 10
-
-###################### test run to drop variables #################
-t = proc.time()
-fit_test = XBART.multinomial(y=matrix(y), num_class=10, X=X_train, Xtest=X_test, 
-                        num_trees=num_trees, num_sweeps=2, max_depth=max_depth, 
-                        Nmin=Nmin, num_cutpoints=num_cutpoints, alpha=0.95, beta=1.25, tau_a = 1.5, tau_b = 0.5,
-                        no_split_penality = 1, weight = seq(1, 10, 0.5), burnin = 1, mtry = mtry, p_categorical = p, 
-                        kap = 1, s = 1, verbose = TRUE, parallel = TRUE, set_random_seed = TRUE, 
-                        random_seed = NULL, sample_weights_flag = TRUE, sample_per_tree = TRUE, stop_threshold = 0.1) 
-t = proc.time() - t
-cat("test fit running time ", t[3], " seconds \n")
-
-X_train = X_train[, -which(fit_test$importance < drop_threshold)]
-X_test = X_test[, -which(fit_test$importance < drop_threshold)]
-p = ncol(X_train)
-cat('dropped variables ', which(fit_test$importance < drop_threshold) )
-##################################################################
+# drop_threshold = 10
+# 
+# ###################### test run to drop variables #################
+# t = proc.time()
+# fit_test = XBART.multinomial(y=matrix(y), num_class=10, X=X_train, Xtest=X_test, 
+#                         num_trees=num_trees, num_sweeps=2, max_depth=max_depth, 
+#                         Nmin=Nmin, num_cutpoints=num_cutpoints, alpha=0.95, beta=1.25, tau_a = 1.5, tau_b = 0.5,
+#                         no_split_penality = 1, weight = seq(1, 10, 0.5), burnin = 1, mtry = mtry, p_categorical = p, 
+#                         kap = 1, s = 1, verbose = TRUE, parallel = TRUE, set_random_seed = TRUE, 
+#                         random_seed = NULL, sample_weights_flag = TRUE, sample_per_tree = TRUE, stop_threshold = 0.1) 
+# t = proc.time() - t
+# cat("test fit running time ", t[3], " seconds \n")
+# 
+# X_train = X_train[, -which(fit_test$importance < drop_threshold)]
+# X_test = X_test[, -which(fit_test$importance < drop_threshold)]
+# p = ncol(X_train)
+# cat('dropped variables ', which(fit_test$importance < drop_threshold) )
+# ##################################################################
 
 
 ###################### parallel #################
