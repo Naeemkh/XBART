331ef706d27fd7d5196e81a87adaad9ce16fe94f
MaggieWang0614 <maggiewang@asu.edu>
2020-05-29 15:45:12 -0700

    change paramters for mnist

--- tests/test_mnist.R
+++ tests/test_mnist.R
@@ -77,8 +77,8 @@ num_cutpoints=20
 t = proc.time()
 fit = XBART.multinomial(y=matrix(y), num_class=10, X=X_train, Xtest=X_test, 
                         num_trees=num_trees, num_sweeps=num_sweeps, max_depth=max_depth, 
-                        Nmin=Nmin, num_cutpoints=num_cutpoints, alpha=0.95, beta=1.25, tau_a = 1, tau_b = 1, 
-                        no_split_penality = 1, weight = seq(1, 10, 0.5), burnin = burnin, mtry = mtry, p_categorical = p, 
+                        Nmin=Nmin, num_cutpoints=num_cutpoints, alpha=0.95, beta=1.25, tau_a = 2, tau_b = 2, 
+                        no_split_penality = 1, weight = c(1), burnin = burnin, mtry = mtry, p_categorical = p, 
                         kap = 1, s = 1, verbose = TRUE, parallel = TRUE, set_random_seed = TRUE, 
                         random_seed = NULL, sample_weights_flag = TRUE, stop_threshold = 0.1, nthread = 0) 
 t = proc.time() - t
@@ -104,4 +104,4 @@ cat(paste("xbart logloss : ",round(logloss,3)),"\n")
 #       " misclassified as ", tail(names(sort(table(yhat[ytest==i]))), 2)[1], "\n " )
 # }
 # 
-saveRDS(fit, paste(path, 'mnist_result/mnist_categorical_sample_per_tree.rds', sep = ''))
+saveRDS(fit, paste(path, 'mnist_result/mnist_categorical_weight_per_tree.rds', sep = ''))
--- tests/test_multinomial_prh.R
+++ tests/test_multinomial_prh.R
@@ -86,7 +86,7 @@ num_trees = 20
 tm = proc.time()
 fit = XBART.multinomial(y=matrix(y_train), num_class=k, X=X_train, Xtest=X_test, 
                         num_trees=num_trees, num_sweeps=num_sweeps, max_depth=8, 
-                        Nmin=10, num_cutpoints=100, alpha=0.95, beta=1.25, tau_a = num_trees/2, tau_b = num_trees/2, 
+                        Nmin=10, num_cutpoints=100, alpha=0.95, beta=1.25, tau_a = 2, tau_b = 2,#tau_a = num_trees/2, tau_b = num_trees/2, 
                          # tau_a can't be 1 or mx = 0 would raise problem; 
                         no_split_penality = 1, weight = seq(1.5, 10, 0.5),
                         burnin = burnin, mtry = 3, p_categorical = p_cat, 
